92e6514726a127f36c82f7311345edac47a232cf
Maduki-tech <d.schlueter1011@gmail.com>
2026-01-05 22:30:40 +0100

    feat: Added Logging

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct{}
 
 func main() {
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println("fatal:", err)
+		os.Exit(1)
+	}
+	defer f.Close()
 	p := tea.NewProgram(model{}, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		panic(err)
@@ -25,11 +34,13 @@ func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q", "esc":
 			return m, tea.Quit
+		case "enter":
+			log.Println("Enter Pressed")
 		}
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	return fmt.Sprintf("Hello %s", "hi")
+	return lipgloss.JoinHorizontal(lipgloss.Top, "Hello", "Hi")
 }
